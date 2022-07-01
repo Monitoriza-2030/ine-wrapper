e1898cb8704f0e97c62eb0b382c488cd66697c25
António Bento <a.f.bento@ua.pt>
2022-07-01 01:58:15 +0100

    inserted cors and updated external port

--- api/server.py
+++ api/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from indicators import Index
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
 @app.get('/index/<code>')
